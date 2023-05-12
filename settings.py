4f6a0defbbc0f6167f323ca6b7ab41be0e03e449
sheffkmv <123307790+sheffkmv@users.noreply.github.com>
2023-05-12 11:27:54 +0300

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,9 @@
+valid_email = 'fortesman@gmail.com'
+valid_password = 'qwerty'
+
+invalid_email = 'zimanbek@yandex.ru'
+invalid_password = '12345'
+Footer
+© 2023 GitHub, Inc.
+Footer navigation
+Terms
